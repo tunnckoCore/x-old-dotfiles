767d978674fdb2d43c31d5eb0734065ec959525a
tunnckoCore <mameto_100@mail.bg>
2016-06-23 13:32:37 +0300

    cleanup

--- i3/scripts/dmenufm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# A fuzzy file-finder and opener based on dmenu
-# Requires: dmenu, exo-open
-cat ~/.cache/dmenufm_cache | dmenu -i -l 5 -fn "Noto Sans 11"  -nb "#1f222d" -nf "#ffffff"  -sf "#dc322f" -sb "#073642" | xargs exo-open
