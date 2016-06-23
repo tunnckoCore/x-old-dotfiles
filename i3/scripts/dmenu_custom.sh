e0c793695e19f148bfe0477e1d6aa768bcb351ee
tunnckoCore <mameto_100@mail.bg>
2016-06-23 05:22:43 +0300

    i3 config

---
+++ i3/scripts/dmenu_custom.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec dmenu -fn "Noto Sans 11"  -nb "#1f222d" -nf "#ffffff"
---
+++ i3/scripts/volume.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+vol=$(amixer get 'Master' | grep Mono: | sed 's/ *Mono: .* \[\([^ ]*\)\] \[[^ ]*\] \[\([^ ]*\)\]/\1 \2/;s/.*off/off/;s/\([^ ]*\).*/\1/')
+echo $vol
