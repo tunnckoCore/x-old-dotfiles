c7a0c88402b93a8a087e6f6b2ef37c4e235363df
tunnckoCore <mameto_100@mail.bg>
2016-06-23 13:30:32 +0300

    final i3 + scripts

---
+++ .xinitrc.i3
@@ -0,0 +1,3 @@
+setxkbmap -layout "us,bg(phonetic)" -option "grp:alt_shift_toggle" &
+numlockx on &
+exec i3 -c ~/.config/i3/config.conf
--- i3/scripts/dmenu_custom.sh
+++ i3/scripts/dmenu_custom.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec dmenu -fn "Noto Sans 11"  -nb "#1f222d" -nf "#ffffff"
+# dmenu2 on AUR using i3-dmenu-desktop --dmenu option
+dmenu_run -nb "#1f222d" -sb "#1f222d" -sf "#7780a1" -fn "Noto Sans-12"  -b -h 25 -x 129 -y 1 -w 730
