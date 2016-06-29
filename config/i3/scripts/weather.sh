21777885c2a0a8144d06a467654fe14c3a270210
tunnckoCore <mameto_100@mail.bg>
2016-06-29 21:36:18 +0300

    update weather

--- config/i3/scripts/weather.sh
+++ config/i3/scripts/weather.sh
@@ -10,14 +10,14 @@ fi
 
 # 0 or 1; 0 for F, 1 for C; default 1
 METRIC=${2:-1}
-
+# $1 idva ot i3blocks
 curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
 {
 print "";
 }
 if( $values[0] eq "Mostly Cloudy" || $values[0] eq "Cloudy" || $values[0] eq "Dreary (Overcast)" || $values[0] eq "Fog")
 {
-print "";
+print " ";
 }
 if( $values[0] eq "Showers" || $values[0] eq "Mostly Cloudy w/ Showers" || $values[0] eq "Partly Sunny w/ Showers" || $values[0] eq "T-Storms"|| $values[0] eq "Mostly Cloudy w/ T-Storms"|| $values[0] eq "Partly Sunny w/ T-Storms"|| $values[0] eq "Rain")
 {
